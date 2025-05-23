8bfe46927aac1781378603f503cfb06a39d7a73a
Julien Cotte <Julien.Cotte-CIC@ibm.com>
2025-05-23 17:06:43 +0200

    Add CustomTabBar, remove addrepomodal button from RepoListView

--- OctoTrack/Views/AddRepositoryModal.swift
+++ OctoTrack/Views/AddRepositoryModal.swift
@@ -10,72 +10,79 @@ import SwiftUI
 struct AddRepositoryModal: View {
     @State private var viewModel: AddRepoViewModel
     @Environment(\.dismiss) private var dismiss
-
+    
     init(dataManager: UserDataManager) {
-           self._viewModel = State(initialValue: AddRepoViewModel(dataManager: dataManager))
-       }
-
+        self._viewModel = State(initialValue: AddRepoViewModel(dataManager: dataManager))
+    }
+    
     var body: some View {
-        VStack(spacing: 20) {
-            Text("repoAdd")
-                .font(.headline)
-                .padding(.top)
-
-            CustomTextField(
-                header: "owner",
-                color: .gray,
-                placeholder: "ownerExemple",
-                text: $viewModel.owner,
-                type: .alphaNumber
-            )
-
-            CustomTextField(
-                header: "repoName",
-                color: .gray,
-                placeholder: "repoExemple",
-                text: $viewModel.repoName,
-                type: .alphaNumber
-            )
-
-            VStack(alignment: .leading, spacing: 10) {
-                Text("priority")
-                    .fontWeight(.bold)
-                    .padding(.horizontal, 4)
-                PriorityButtonsStack(selectedPriority: $viewModel.priority)
-            }
-
-            if viewModel.isLoading {
-                ProgressView()
-                    .padding()
-            } else {
-                Button(action: {
-                    Task {
-                         await viewModel.getRepo()
-                    }
-                },
-                label: {
-                    CustomButtonLabel(
-                        iconLeading: .plus,
-                        message: "repoAdd",
-                        color: .green
+        NavigationView {
+            VStack(spacing: 20) {
+                Text("repoAdd")
+                    .font(.headline)
+                    .padding(.top)
+                
+                CustomTextField(header: String(localized: "owner"),
+                                color: .gray,
+                                placeholder: "ownerExemple",
+                                text: $viewModel.owner,
+                                type: .alphaNumber
+                )
+                
+                CustomTextField(header: String(localized: "repoName"),
+                                color: .gray,
+                                placeholder: "repoExemple",
+                                text: $viewModel.repoName,
+                                type: .alphaNumber
+                )
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("priority")
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 4)
+                    PriorityButtonsStack(selectedPriority: $viewModel.priority)
+                }
+                
+                if viewModel.isLoading {
+                    ProgressView()
+                        .padding()
+                } else {
+                    Button(action: {
+                        Task {
+                            await viewModel.getRepo()
+                        }
+                    },
+                           label: {
+                        CustomButtonLabel(iconLeading: .plus,
+                                          message: "repoAdd",
+                                          color: .green
                         )
                     }
-                )
-                // Disable button while form is not valid
-                .disabled(!viewModel.isFormValid)
-                .opacity(viewModel.isFormValid ? 1 : 0.6)
+                    )
+                    // Disable button while form is not valid
+                    .disabled(!viewModel.isFormValid)
+                    .opacity(viewModel.isFormValid ? 1 : 0.6)
+                }
+                
+                if viewModel.feedback != .none {
+                    FeedbackLabel(feedback: viewModel.feedback)
+                }
+                Spacer()
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button("close") {
+                                dismiss()
+                            }
+                        }
+                    }
             }
-
-            if viewModel.feedback != .none {
-                FeedbackLabel(feedback: viewModel.feedback)
-            }
-            Spacer()
-        }
-        .onChange(of: viewModel.feedback) {
-            if !viewModel.feedback.isError {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                    viewModel.feedback = .none
-                    dismiss()
+            .onChange(of: viewModel.feedback) {
+                if !viewModel.feedback.isError {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                        viewModel.feedback = .none
+                        dismiss()
+                    }
                 }
             }
         }
